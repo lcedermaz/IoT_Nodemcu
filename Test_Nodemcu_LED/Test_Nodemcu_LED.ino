4db6a1d7835abfbd44caaf3287d93fff82e84d35
lcedermaz <32801806+lcedermaz@users.noreply.github.com>
2018-07-22 01:07:01 -0300

    Agregado los archivos
    la denominación LED corresponde al de la primer prueba, los demás archivos son del tipo IoT, con conexión al servidor a: https://thingspeak.com/

---
+++ Test_Nodemcu_LED/Test_Nodemcu_LED.ino
@@ -0,0 +1,31 @@
+/*#define LED_BUILTIN 2    //
+int ledState = LOW ; // Estado inicial para el parpadeo del led indicador de grabación
+
+unsigned long previousMillis = 0;
+const long interval = 500 ;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+  Toggle_LED ();
+
+}
+
+void Toggle_LED () {
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(LED_BUILTIN, ledState);
+  }
+}*/
---
+++ Test_Nodemcu_LED/Test_Servidor_Thingspeak.ino
@@ -0,0 +1,81 @@
+/*
+ ESP8266 --> ThingSpeak Channel via MKR1000 Wi-Fi
+ 
+ This sketch sends the value of Analog Input (A0) to a ThingSpeak channel
+ using the ThingSpeak API (https://www.mathworks.com/help/thingspeak).
+ 
+ Requirements:
+ 
+   * ESP8266 Wi-Fi Device
+   * Arduino 1.6.9+ IDE
+   * Additional Boards URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
+   * Library: esp8266 by ESP8266 Community
+ 
+ ThingSpeak Setup:
+ 
+   * Sign Up for New User Account - https://thingspeak.com/users/sign_up
+   * Create a new Channel by selecting Channels, My Channels, and then New Channel
+   * Enable one field
+   * Note the Channel ID and Write API Key
+    
+ Setup Wi-Fi:
+  * Enter SSID
+  * Enter Password
+  
+ Tutorial: http://nothans.com/measure-wi-fi-signal-levels-with-the-esp8266-and-thingspeak
+   
+ Created: Feb 3, 2017 by Hans Scharler (http://nothans.com)
+*/
+
+#include <ESP8266WiFi.h>
+
+// Wi-Fi Settings
+const char* ssid = "Red-Casa"; // your wireless network name (SSID)
+const char* password = "lceder2872"; // your Wi-Fi network password
+
+WiFiClient client;
+
+// ThingSpeak Settings
+const int channelID = 544264;
+String writeAPIKey = "NUBYH45AO92Q6I50"; // write API key for your ThingSpeak Channel
+const char* server = "api.thingspeak.com";
+const int postingInterval = 20 * 1000; // post data every 20 seconds
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if (client.connect(server, 80)) {
+    
+    // Measure Analog Input (A0)
+    int valueA0 = analogRead(A0);
+
+    // Construct API request body
+    String body = "field1=";
+           body += String(valueA0);
+    
+    Serial.print("A0: ");
+    Serial.println(valueA0); 
+
+    client.println("POST /update HTTP/1.1");
+    client.println("Host: api.thingspeak.com");
+    client.println("User-Agent: ESP8266 (nothans)/1.0");
+    client.println("Connection: close");
+    client.println("X-THINGSPEAKAPIKEY: " + writeAPIKey);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Content-Length: " + String(body.length()));
+    client.println("");
+    client.print(body);
+
+  }
+  client.stop();
+
+  // wait and then post again
+  delay(postingInterval);
+}
